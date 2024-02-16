12934e5502301ba69b390bda22417946929dc062
Amappola <anam.porrasp@gmail.com>
2024-02-15 21:29:57 -0500

    Creating nav bar structure

---
+++ src/app/ui/shared/nav-bar/navBarUi.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function navBarUi() {
+  return (
+    <nav className='nav-bar'>
+      <ul>
+        <li>
+          <a>Inicio</a>
+        </li>
+        <li>
+          <a>Comprar Boletos</a>
+        </li>
+        <li>
+          <a>Mis Boletos</a>
+        </li>
+        <li>
+          <a>Contacto</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default navBarUi
